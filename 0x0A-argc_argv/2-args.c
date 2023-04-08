dd0d9329d2658bea7614872183f1b5b581ebbf4a
princewilliyk <Uka Princewill>
2023-04-08 00:52:13 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-#include "main.h"
-int main(int argc, char* argv[]){
- int i;
- for (i = 0; i < argc; i++){
- printf("%s\n", argv[i]);
- return 0;
- }
+
+/**
+ * main - entry point
+ * Description: prints all command line arguements
+ * @argc: the count of arguements
+ * @argv: the array of arguements
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
