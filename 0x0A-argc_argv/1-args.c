597b661184ccb329fe21e6ecf3d718de34b0c774
princewilliyk <Uka Princewill>
2023-04-10 04:56:27 -0700

    task 01

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main (int argc, char* argv[]__attribute__((unused))){
-    printf("%d\n", argc - 1);
-    return 0;
+
+/**
+ * main - entry point
+ * Description: prints number of command line arguements
+ * @argc: number of arguements
+ * @argv: array of arguemnts
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
 }
