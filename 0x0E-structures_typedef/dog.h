11786f30b132fc6bb8add9b8ae730028fbd87d0f
princewilliyk <Uka Princewill>
2023-04-18 17:57:05 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,14 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * dog_t - new name (typedef) for dog struct.
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
