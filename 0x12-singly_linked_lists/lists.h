c6b4e043288d4f60769f8aeb64876be866657864
princewilliyk <Uka Princewill>
2023-04-27 15:45:58 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
 /**
  * struct list_s - singly linked list
  * @str: string
- * @len length of string
+ * @len: length of string
  * @next: point next node
  * Description: singly linked list struct
  */
