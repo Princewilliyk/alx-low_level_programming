a7c0ad0e2bc4558fd34e132b1c8cb9d858462df2
princewilliyk <Uka Princewill>
2023-04-27 15:43:51 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len length of string
+ * @next: point next node
+ * Description: singly linked list struct
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
