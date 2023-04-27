a0459218a748addab60a31c9a5818c52e8fe9d8f
princewilliyk <Uka Princewill>
2023-04-27 15:30:14 -0700

    task 0

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,14 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+
+#endif
