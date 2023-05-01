acb25aad7fdcff31825199174995de8a1c7a11e1
princewilliyk <Uka Princewill>
2023-05-01 09:31:53 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  *print_listint - prints the content of a list
  *@h: the head pointer
@@ -8,7 +9,7 @@ size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
-	while (h)
+	if (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
