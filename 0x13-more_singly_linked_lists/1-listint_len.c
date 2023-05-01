952db749fafcabc9439ff4cb3fdc3a1fb3be826d
princewilliyk <Uka Princewill>
2023-05-01 09:47:23 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *listint_len - returns the amount of elements in a list
+ *@h: head of node
+ *Return: returns the count
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
