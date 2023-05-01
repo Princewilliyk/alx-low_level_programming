9a85d3ab02158d96f111bd01ce460f72a94a7f8e
princewilliyk <Uka Princewill>
2023-05-01 09:12:30 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ *print_listint - prints the content of a list
+ *@h: the head pointer
+ *Return: returns the amount of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
