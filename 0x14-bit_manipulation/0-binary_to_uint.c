0b90a77eac0ac236b260840eece8e916e3fa8adc
princewilliyk <Uka Princewill>
2023-05-04 19:43:01 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,14 @@ unsigned int binary_to_uint(const char *b);
 	unsigned int t = 0;
 	unsigned int rsult = 0;
 
-	while (b[t] != '0' && b[t] != '1' && b[t] != '\0')
+	while (b[t] != '\0')
 	{
-		return (0);
-		rsult <<= 1;
+		if (b[t] != '0' && b[t] != '1')
+			return (0);
+			rsult <<= 1;
+		if (b[t] & 1)
+			rsult += 1;
+		t += 1;
 	}
-	if (b[t] & 1)
-		rsult += 1;
-	t += 1
 	return (rsult);
 }
