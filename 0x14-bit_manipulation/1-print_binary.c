9fe3e6d13e3301b94fd51b40a3040c8cb00427c8
princewilliyk <Uka Princewill>
2023-05-04 21:46:54 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -9,16 +8,11 @@
 
 void print_binary(unsigned long int n)
 {
-	/* check if n is zero, if n is zero, binary representation is also zero*/
-
 	if ((n >> 1) == 0 && n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-
-	/*check if n still has some binary digits to print*/
-
 	if (n >> 1 != 0)
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
